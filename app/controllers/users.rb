b7697a129d87880539787138f7789370f88b0785
jeansampen <jeansampen93@gmail.com>
2015-05-01 16:02:53 -0500

    Most of the user login/logout/authentication crap

--- app/controllers/users.rb
+++ app/controllers/users.rb
@@ -10,3 +10,26 @@ get '/login' do
   end
 end
 
+get '/users/new' do
+  erb :"users/create"
+end
+
+post '/login' do
+  session[:user_id] = User.authenticate(params[:user])
+  if logged_in?
+    redirect '/decks'
+  else
+    @message = "Username/password combination not found"
+    erb :"users/login"
+  end
+end
+
+post '/users' do
+  session[:user_id] = User.create(params[:user]).id
+  redirect '/decks'
+end
+
+get '/logout' do
+  session[:user_id] = nil
+  redirect '/login'
+end
--- app/helpers/user_helpers.rb
+++ app/helpers/user_helpers.rb
@@ -2,4 +2,10 @@ helpers do
   def logged_in?
     !!session[:user_id]
   end
+
+  def login
+
+  end
+
+
 end
