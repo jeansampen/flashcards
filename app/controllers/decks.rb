49251178701a8c7ed52dc894aee9549aea3e62e3
jeansampen <jeansampen93@gmail.com>
2015-05-01 16:29:46 -0500

    comment out post attempts

--- app/controllers/decks.rb
+++ app/controllers/decks.rb
@@ -32,15 +32,15 @@ get '/rounds/:id' do
   end
 end
 
-post '/attempts' do
-  round = Round.find(session[:round_id])
-  @correctness = ()
-  round.attempts.create(card_id: sesson[:current_card][:id],
-    )
-  # creates an attempt
-  # @correctness = attempt.correct
-  erb :'cards/result'
-end
+# post '/attempts' do
+#   round = Round.find(session[:round_id])
+#   @correctness = ()
+#   round.attempts.create(card_id: session[:current_card][:id],
+#     )
+#   # creates an attempt
+#   # @correctness = attempt.correct
+#   erb :'cards/result'
+# end
 
 get '/stats' do
   # displays all rounds w/ all stats
